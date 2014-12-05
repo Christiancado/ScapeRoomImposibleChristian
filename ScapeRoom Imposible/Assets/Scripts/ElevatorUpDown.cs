e5fe13ff68cdff1bdcbdc6d426a61875c0d4323e
Christiancado <christian.cadorin@live.u-tad.com>
2014-12-05 23:12:49 +0100

    Tercer Commit ScapeRoom
    Faltaba un comentario en un Script

--- ScapeRoom Imposible/Assets/Scripts/ElevatorUpDown.cs
+++ ScapeRoom Imposible/Assets/Scripts/ElevatorUpDown.cs
@@ -11,14 +11,20 @@ public class ElevatorUpDown : MonoBehaviour {
 		elevator = GameObject.Find("ElevatorMovement") as GameObject;
 		updAndDown = elevator.GetComponent<Animation>();
 	}
-	
+
+	/*Condiciona el objeto ElevatorMovement para que solo suba si esta en una posicion 
+	 menor o igual que 3.4 que es la altura maxima a la que se eleva*/
 	public void Up(){
 		y = elevator.transform.position.y;
-		if (y >= 0) {
+		if (y >= 0 && y <= 3.4) {
+
 			updAndDown.Play ("elevatorUp");
 		}
 	}
 
+
+	/*Se asegura que el ascensor solo baje si se encuentra a una altura mayor o igual
+	 que el tope al que debe llegar*/
 	public void Down(){
 		y = elevator.transform.position.y;
 		if(y >= 3.4){
@@ -26,6 +32,9 @@ public class ElevatorUpDown : MonoBehaviour {
 		}
 	}
 
+	/*Me aseguro de que el objeto baje cuando el personaje salga del trigger
+	 , es decir, cuando llegue arriba del todo siempre y cuando el ascensor
+	 no este ya abajo*/
 	private void OnTriggerExit(Collider collider){
 		y = elevator.transform.position.y;
 		if(collider.name == "Player" && y > 0){
